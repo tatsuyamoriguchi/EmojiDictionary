4de23113afe67882685ff87e73f6b4177fc4e234
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-10-25 12:34:25 -0700

    p.579 Handle Delete

--- EmojiDictionary/EmojiCollectionViewController.swift
+++ EmojiDictionary/EmojiCollectionViewController.swift
@@ -104,4 +104,24 @@ class EmojiCollectionViewController: UICollectionViewController {
         
     }
 
+
+    
+    override func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let config = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { (elements) -> UIMenu? in
+            let delete = UIAction(title: "Delete") { (action) in
+                self.deleteEmoji(at: indexPath)
+            }
+
+            return UIMenu(title: "", image: nil, identifier: nil, options: [], children: [delete])
+        }
+        return config
+    }
+
+    
+    func deleteEmoji(at indexPath: IndexPath) {
+        emojis.remove(at: indexPath.row)
+        collectionView.deleteItems(at: [indexPath])
+    }
+    
+
 }
